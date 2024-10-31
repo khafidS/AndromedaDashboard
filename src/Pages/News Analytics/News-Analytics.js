61867c06d0fadb82fe10312526a292713b0d8ee8
caveinjoe <99408687+caveinjoe@users.noreply.github.com>
2024-10-31 15:52:00 +0700

    Update link

--- src/Pages/News Analytics/News-Analytics.js
+++ src/Pages/News Analytics/News-Analytics.js
@@ -15,9 +15,9 @@ const NewsAnalytics = () => {
                 <Container fluid={true}>
                     <Breadcrumbs title="VisionAnalytics.AI" breadcrumbItem="News Analytics" />
                     <div style={{width: '100%', height: '100vh'}}>
-                    <iframe src="
-                    http://useast.services.cloud.techzone.ibm.com:24137/bi/?perspective=dashboard&amp;pathRef=.my_folders%2FMultipolar%2FModified%2BEnglish%2FCombined%2BUse%2Bcase%2B2%2B%2526%2B3%2FNews%2BAnalytics%2BDashboard%2BEnglish&amp;closeWindowOnLastView=true&amp;ui_appbar=false&amp;ui_navbar=false&amp;shareMode=embedded&amp;action=view&amp;mode=dashboard&amp;subView=model000001926669fd34_00000000"
-                    width="100%" height="100%" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen=""></iframe>
+                        <iframe src="
+                        http://useast.services.cloud.techzone.ibm.com:47741/bi/?perspective=dashboard&amp;pathRef=.my_folders%2FMultipolar%2FModified%2BEnglish%2FCombined%2BUse%2Bcase%2B2%2B%2526%2B3%2FNews%2BAnalytics%2BDashboard%2BEnglish&amp;closeWindowOnLastView=true&amp;ui_appbar=false&amp;ui_navbar=false&amp;shareMode=embedded&amp;action=view&amp;mode=dashboard&amp;subView=model000001926669fd34_00000000" 
+                        width="100%" height="100%" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen=""></iframe>
                     </div>
                 </Container>
             </div>
--- src/Pages/SocialMediaAnalytics/Social-Media-Analytics.js
+++ src/Pages/SocialMediaAnalytics/Social-Media-Analytics.js
@@ -16,7 +16,7 @@ const SocialMediaAnalytics = () => {
                     <Breadcrumbs title="VisionAnalytics.AI" breadcrumbItem="Social Media Analytics" />
                     <div style={{width: '100%', height: '100vh'}}>
                         <iframe src="
-                        http://useast.services.cloud.techzone.ibm.com:24137/bi/?perspective=dashboard&amp;pathRef=.my_folders%2FMultipolar%2FModified%2BEnglish%2FUse%2Bcase%2B1%2FSocial%2BMedia%2BDashboard%2BEnglish&amp;closeWindowOnLastView=true&amp;ui_appbar=false&amp;ui_navbar=false&amp;shareMode=embedded&amp;action=view&amp;mode=dashboard&amp;subView=model000001922d757799_00000000"
+                        http://useast.services.cloud.techzone.ibm.com:47741/bi/?perspective=dashboard&amp;pathRef=.my_folders%2FMultipolar%2FModified%2BEnglish%2FUse%2Bcase%2B1%2FSocial%2BMedia%2BDashboard%2BEnglish&amp;closeWindowOnLastView=true&amp;ui_appbar=false&amp;ui_navbar=false&amp;shareMode=embedded&amp;action=view&amp;mode=dashboard&amp;subView=model000001922d356d5d_00000002" 
                         width="100%" height="100%" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen=""></iframe>
                     </div>
                 </Container>
